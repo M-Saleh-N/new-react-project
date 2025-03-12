d689098c6d7f4301694b87d17f30092b35acb22e
M-Saleh-N <salehnasser65784@gmail.com>
2025-03-12 17:13:13 +0300

    added a form on the login page

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,5 +1,13 @@
 const LoginComponent = () => {
     return(
-        <div></div>
+        <div>
+            <label for="Email"></label>
+            <input id="email" name="email" type="text" placeholder="Email" required />
+            <label for="Password"></label>
+            <input id="password" name="Password" type="password" placeholder="Password" required />
+            <button id="submitButton">SUBMIT</button>
+        </div>
     )
 }
+
+export default LoginComponent
