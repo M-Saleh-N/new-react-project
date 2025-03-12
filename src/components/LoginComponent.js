d6df25e08816e37471abe898a68c4579826c11b9
M-Saleh-N <salehnasser65784@gmail.com>
2025-03-12 17:17:40 +0300

    made changes

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,7 +1,7 @@
 const LoginComponent = () => {
     return(
         <div>
-            <H1>LOGIN</H1>
+            <h1>LOGIN</h1>
             <label for="Email"></label>
             <input id="email" name="email" type="text" placeholder="Email" required />
             <label for="Password"></label>
