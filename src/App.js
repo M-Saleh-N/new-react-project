31e840218a5dceaec0b92bec889beadcb714fff3
M-Saleh-N <salehnasser65784@gmail.com>
2025-03-12 17:15:08 +0300

    imported the logincomponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import FormComponent from './components/formComponent';
+import LoginComponent from './components/LoginComponent';
 
 function App() {
   return (
     <div>
       <FormComponent/>
+      <LoginComponent/>
     </div>
   );
 }
